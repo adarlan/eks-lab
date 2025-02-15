766b335a6780267cdd2ab3bd5631bc0f4b5fba43
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-02-14 21:14:11 -0300

    Refactor cluster-tools modules

--- cluster-tools/main.tf
+++ cluster-tools/main.tf
@@ -1,17 +1,17 @@
 module "route53_zone" {
-  source   = "./modules/route53-zone"
+  source   = "./route53-zone"
   for_each = var.create_route53_zone ? { "${var.domain_name}" = {} } : {}
 
   domain_name = var.domain_name
 }
 
 module "ingress_nginx" {
-  source     = "./modules/ingress-nginx"
+  source     = "./ingress-nginx"
   depends_on = [module.route53_zone]
 }
 
 module "dns_records" {
-  source     = "./modules/dns-records"
+  source     = "./dns-records"
   depends_on = [module.route53_zone, module.ingress_nginx]
 
   registered_domain = var.domain_name
@@ -27,13 +27,13 @@ module "dns_records" {
 }
 
 module "oidc_provider" {
-  source = "./modules/oidc-provider"
+  source = "./oidc-provider"
 
   cluster_name = var.cluster_name
 }
 
 module "cert_manager" {
-  source = "./modules/cert-manager"
+  source = "./cert-manager"
 
   registered_domain = var.domain_name
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn
@@ -41,7 +41,7 @@ module "cert_manager" {
 }
 
 module "letsencrypt_issuers" {
-  source     = "./modules/letsencrypt-issuers"
+  source     = "./letsencrypt-issuers"
   depends_on = [module.cert_manager]
 
   acme_email        = var.acme_email
@@ -50,7 +50,7 @@ module "letsencrypt_issuers" {
 
 
 # module "acm_certificate" {
-#   source     = "./modules/acm-certificate"
+#   source     = "./acm-certificate"
 #   depends_on = [module.route53_zone]
 
 #   domain_name = var.domain_name
@@ -60,7 +60,7 @@ module "letsencrypt_issuers" {
 # }
 
 # module "ingress_nginx" {
-#   source     = "./modules/ingress-nginx-aws-certificate-manager"
+#   source     = "./ingress-nginx-aws-certificate-manager"
 #   depends_on = [module.route53_zone, module.acm_certificate]
 
 #   domain_name = var.domain_name
@@ -71,7 +71,7 @@ module "letsencrypt_issuers" {
 # }
 
 # module "ingress_dns" {
-#   source = "./modules/elb-dns-record"
+#   source = "./elb-dns-record"
 #   depends_on = [ module.ingress_nginx ]
 #   registered_domain = var.registered_domain
 #   subdomains = [
@@ -80,14 +80,14 @@ module "letsencrypt_issuers" {
 # }
 
 # module "argocd" {
-#   source     = "./modules/argocd"
+#   source     = "./argocd"
 #   depends_on = [module.ingress_nginx]
 
 #   argocd_ingress_host = "argocd.${var.domain_name}"
 # }
 
 # module "loki" {
-#   source = "./modules/loki-simple-scalable"
+#   source = "./loki-simple-scalable"
 
 #   aws_s3_bucket_name         = var.loki_s3_bucket_name
 #   aws_iam_user_name          = "${var.cluster_name}-loki"
@@ -96,7 +96,7 @@ module "letsencrypt_issuers" {
 # }
 
 # module "kube_prometheus_stack" {
-#   source     = "./modules/kube-prometheus-stack"
+#   source     = "./kube-prometheus-stack"
 #   depends_on = [module.ingress_nginx]
 
 #   grafana_ingress_host    = "grafana.${var.domain_name}"
