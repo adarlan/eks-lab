780a8ae3cd1808c43ed05acdcab830747d14bfca
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-02-14 21:03:07 -0300

    Update cluster module

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,36 +1,25 @@
-module "vpc" {
-  source = "./vpc"
-
-  vpc_name       = var.vpc_name
-  vpc_cidr_block = var.vpc_cidr_block
-}
-
-module "availability_zones" {
-  source = "./availability-zones"
-
-  max_selected_zones = 2
-}
-
-module "subnets" {
-  source     = "./subnets"
-  depends_on = [module.vpc, module.availability_zones]
+module "vpc_network" {
+  source = "./vpc-network"
 
   eks_cluster_name          = var.cluster_name
-  vpc_id                    = module.vpc.vpc_id
   vpc_cidr_block            = var.vpc_cidr_block
+  max_selected_zones        = 2
   subnet_cidr_block_newbits = 8
-  internet_gateway_id       = module.vpc.internet_gateway_id
   max_nat_gateway_count     = 1
-  availability_zones        = module.availability_zones.selected_zones
 }
 
-# module "control_plane" {
-#   source       = "./control-plane"
-#   depends_on   = [module.vpc, module.subnets]
-#   cluster_name = var.cluster_name
-#   vpc_id       = module.vpc.vpc_id
-#   subnet_ids   = concat(module.subnets.public_subnet_ids, module.subnets.private_subnet_ids)
-# }
+module "control_plane" {
+  source     = "./control-plane"
+  depends_on = [module.vpc_network]
+
+  cluster_name = var.cluster_name
+  vpc_id       = module.vpc_network.vpc_id
+
+  subnet_ids = concat(
+    module.vpc_network.public_subnet_ids,
+    module.vpc_network.private_subnet_ids,
+  )
+}
 
 # module "node_group_role" {
 #   source    = "./node-group-role"
--- cluster/variables.tf
+++ cluster/variables.tf
@@ -23,3 +23,13 @@ variable "aws_default_tags" {
 variable "aws_region" {
   type = string
 }
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
