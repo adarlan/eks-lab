02531def32b3a30ae4de594506a450afd737caf6
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-02-25 01:29:47 -0300

    Update scripts

--- terraform-run.sh
+++ terraform-run.sh
@@ -13,18 +13,13 @@ for arg in "$@"; do
     if [[ "$arg" == "--no-init" ]]; then no_init=true; fi
 done
 
-if $destroy; then
-    operation=destroy
-else
-    operation=apply
-fi
-
-terraform_args=$($no_prompt && echo "-auto-approve" || echo "")
-
-./terraform-config.sh --no-prompt --dir $dir
-
 if grep -Fxq "  cloud {}" $dir/terraform.tf; then
-    source $dir/terraform.cloud.env
+
+    gh_variables=$(gh variable list --json name,value  | jq -r 'map({(.name): .value}) | add')
+
+    export TF_CLOUD_ORGANIZATION=$(echo "$gh_variables" | jq -r '.TF_CLOUD_ORGANIZATION')
+    export TF_CLOUD_PROJECT=$(echo "$gh_variables" | jq -r '.TF_CLOUD_PROJECT')
+    export TF_WORKSPACE="$TF_CLOUD_PROJECT-$dir"
 fi
 
 if ! $no_init; then
@@ -35,13 +30,17 @@ if ! $no_init; then
     )
 fi
 
-if [[ "$operation" == "apply" ]]; then
-    echo; echo "Applying $dir"
-elif [[ "$operation" == "destroy" ]]; then
+if $destroy; then
+    operation=destroy
     echo; echo "Destroying $dir"
+else
+    operation=apply
+    echo; echo "Applying $dir"
 fi
+
+approval_mode=$($no_prompt && echo "-auto-approve" || echo "")
 
 (
     set -ex
-    terraform -chdir=$dir $operation $terraform_args
+    terraform -chdir=$dir $operation $approval_mode
 )
