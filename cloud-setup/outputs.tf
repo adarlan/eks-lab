82d4f770714be44c9076aa88ac44ea77c81aac6a
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-03-03 23:29:05 -0300

    Update outputs

---
+++ cloud-setup/outputs.tf
@@ -0,0 +1,3 @@
+output "tls_secrets_bucket_name" {
+  value = aws_s3_bucket.tls_secrets_bucket.bucket
+}
--- cloud-setup/tls-secrets-bucket.tf
+++ cloud-setup/tls-secrets-bucket.tf
@@ -1,7 +1,3 @@
 resource "aws_s3_bucket" "tls_secrets_bucket" {
   bucket = "${var.github_repository}-tls-secrets-${data.aws_caller_identity.current.account_id}"
 }
-
-output "tls_secrets_bucket_name" {
-  value = aws_s3_bucket.tls_secrets_bucket.bucket
-}
