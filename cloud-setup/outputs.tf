8dd7cee98de98656dbe094d05094d4ab19e0598c
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-03-04 00:07:01 -0300

    Add outputs

--- cloud-setup/outputs.tf
+++ cloud-setup/outputs.tf
@@ -1,3 +1,11 @@
 output "tls_secrets_bucket_name" {
   value = aws_s3_bucket.tls_secrets_bucket.bucket
 }
+
+output "cluster_name" {
+  value = var.github_repository # TODO local.cluster_name?
+}
+
+output "aws_region" {
+  value = var.aws_region
+}
